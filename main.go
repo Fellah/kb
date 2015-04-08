836a8b4d4a1d5ea08bdb6d2e6b056ea0ac9fc9a4
Roman Krivetsky <r.krivetsky@gmail.com>
2015-04-09 00:16:26 +0300

    Add function to parse command line arguments.

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"os"
+)
 
+const (
+	// Default content directory.
+	DATA_DIR string = "."
+)
+
+var dataDir string = DATA_DIR
+
+func main() {
+	parseCliArgs()
+}
+
+// Parse command line arguments.
+func parseCliArgs() {
+	flag.Parse()
+	args := flag.Args()
+
+	// Verify data directory.
+	if len(args) > 0 {
+		dir, err := os.Open(args[len(args)-1])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer dir.Close()
+
+		di, err := dir.Stat()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if !di.IsDir() {
+			log.Fatalln(err)
+		}
+
+		dataDir = args[len(args)-1]
+	}
 }
